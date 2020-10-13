1f1a0f1fe3df9ca2bb297fda2a774fec6b7bf9e7
s0809442442 <i19780219111@kimo.com>
2020-10-13 08:56:15 +0800

    upload_first_hw0205.c

--- hw01-02/hw0205.c
+++ hw01-02/hw0205.c
@@ -1,5 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+
+
 
 int main(){
+	int num[5]={0};
 	
+	printf("Please enter 5 cards.");
+
+	while(scanf("%d %d %d %d %d",&num[0],&num[1],&num[2],&num[3],&num[4])!=5){
+		printf("Wrong input.");
+		exit(0);
+	}
+	
+	for(int i=0;i<5;i++){
+		if(num[i]>52 || num[i]<0){
+			printf("Wrong input.");
+			exit(0);
+	
+		}
+	}
+	
+
+
+
+
+
+
+
 }
