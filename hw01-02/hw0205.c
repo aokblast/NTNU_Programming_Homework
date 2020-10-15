5f07a97cf2d7193f2e6c033927b93bde9350ac91
s0809442442 <i19780219111@kimo.com>
2020-10-16 00:55:15 +0800

    test

--- hw01-02/hw0205.c
+++ hw01-02/hw0205.c
@@ -2,7 +2,61 @@
 #include <stdlib.h>
 
 
+int col_judge(int *a,int size_of_array){
+	int color=a[0]/13;
+	for(int i=1;i<size_of_array;i++){
+		if((a[i]/13)!=color){
+			return 0;
+		}
+	}
+	return 1;	
+}
 
+int num_judge(int *a,int size_of_array,int num){
+	int num_arr[13]={0};
+	for(int i=0;i<size_of_array;i++){
+		num_arr[a[i]%13]++;
+	}
+	
+	for(int i=0;i<13;i++){
+		if(num_arr[i]==num){
+			return 1;
+		}
+	}
+	
+	return 0;
+}
+
+int contin_judge(int *a,int size_of_array){
+	int tmp;
+	for(int i=0;i<size_of_array;i++){
+		for(int j=i+1;j<size_of_array;j++){
+			if(a[i]>a[j]){
+				tmp=a[i];
+				a[i]=a[j];
+				a[j]=tmp;
+			}
+		}
+	}
+	int contin=a[0];
+
+	for(int i=1;i<size_of_array;i++){
+		if(a[i]!=contin+1){
+			return 0;
+		}
+		else{
+			contin++;
+		}
+	}
+	return 1;
+
+}
+
+int norm_judge(int *a,int size_of_array){
+	if(col_judge(a) && contin_judge(a)){
+		return "Straight Flush";
+	}	
+}
 
 
 int main(){
@@ -23,11 +77,6 @@ int main(){
 		}
 	}
 	
-
-
-
-
-
 
 
 }
