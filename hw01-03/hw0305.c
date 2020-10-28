10e22452ab62c14b8bbea552c2997d50840f15df
s0809442442 <i19780219111@kimo.com>
2020-10-28 21:49:35 +0800

    upload_hw0305

---
+++ hw01-03/hw0305.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+double input(*const char )
+
+int main(){
+    int ins_amount=0,ann_pay=0,age=0;
+    double ann_return=0,fee_off=0,ins_payment[6];
+    printf("SIP Plan\n");
+    
+
+}
