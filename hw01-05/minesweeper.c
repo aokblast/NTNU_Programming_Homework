a9742c5141ca2b52b340d79ca52d35a35734297c
s0809442442 <i19780219111@kimo.com>
2020-12-13 17:04:49 +0800

    adjust sorting.c

---
+++ hw01-05/minesweeper.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#define i32 int32_t
+
+
--- hw01-05/sorting.c
+++ hw01-05/sorting.c
@@ -10,10 +10,24 @@ void swap(i32 *a,i32 *b){
     *b=tmp;
 }
 
-i32 partition(i32 *arr,i32 front,i32 end,i32 mode){
+i32 cmp(i32 a,i32 b){
+    i32 x=abs(a);
+    i32 y=abs(b);
+    if(x%2==0 && y%2==0){
+        return a>b;    
+    }else if(x%2==1 && y%2==0){
+        return 1;
+    }else if(x%2==0 && y%2==1){
+        return 0;
+    }else{
+        return a<b;
+    }
+}
+
+i32 partition(i32 *arr,i32 front,i32 end,i32 (*cmp)(i32,i32)){
     i32 i=front-1,pivot=arr[end];   
     for(i32 j=front;j<end;j++){
-    if(mode ? arr[j]>pivot : arr[j]<pivot){
+    if(cmp(arr[j],pivot)){
             i++;
             swap(&arr[i],&arr[j]);
         }
@@ -23,33 +37,19 @@ i32 partition(i32 *arr,i32 front,i32 end,i32 mode){
     return i;
 }
 
-void quick_sort(i32 *arr,i32 front,i32 end,i32 mode){
+void quick_sort(i32 *arr,i32 front,i32 end,i32 (*cmp)(i32,i32)){
     if(front<end){
-        i32 pivot=partition(arr,front,end,mode);
-        quick_sort(arr,front,pivot-1,mode);
-        quick_sort(arr,pivot+1,end,mode);
+        i32 pivot=partition(arr,front,end,cmp);
+        quick_sort(arr,front,pivot-1,cmp);
+        quick_sort(arr,pivot+1,end,cmp);
     }
 }
 
 
 void init(i32 *arr,i32 arr_size){
-    i32 arr1[100000]={0},a=0,b=0,arr2[10000]={0};
-    for(int i=0;i<arr_size;i++){
-        if(abs(arr[i])%2==0){
-            arr1[a]=arr[i];
-            a++;
-        }else{
-            arr2[b]=arr[i];
-            b++;
-        }
-    }
-    quick_sort(arr1,0,a-1,1);
-    quick_sort(arr2,0,b-1,0);
-    for(i32 i=0;i<b;i++){
-        printf("%d ",arr2[i]);
-    }
-    for(i32 i=0;i<a;i++){
-        printf("%d ",arr1[i]);
+    quick_sort(arr,0,arr_size-1,cmp);
+    for(i32 i=0;i<arr_size;i++){
+        printf("%d ",arr[i]);
     }
     printf("\n");
     
