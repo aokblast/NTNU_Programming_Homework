d1f5462b9d5760720a82460a057ed3d449cb689a
s0809442442 <i19780219111@kimo.com>
2021-01-10 16:39:31 +0800

    (Code)Adjust showplane.

--- hw01-06/projection.c
+++ hw01-06/projection.c
@@ -8,25 +8,36 @@ void showplane(i32 a,i32 b,i32 c,i32 d){
     printf("The plane is ");
     i32 cnt=0; 
     if(a!=0){
-        if(a==1)printf("x");
+        if(a==1) printf("x");
+        else if(a==-1) printf("-x");
         else printf("%dx",a);
-        cnt++;
+        cnt=1;
     }
     if(b!=0){
-        if(cnt==1) {
+        if(cnt==1 && b>0) {
             printf("+");
             cnt=0;
         }
-        if(b==1)printf("y");
+        if(b==1) printf("y");
+        else if(b==-1) printf("-y");
         else printf("%dy",b);
-        cnt++;
+        cnt=1;
     }
     if(c!=0){
-        if(cnt==1) printf("+");
+        if(cnt==1 && c>0){
+            printf("+");
+            cnt=0;
+        }
         if(c==1) printf("z");
+        else if(c==-1) printf("-z");
         else printf("%dz",c);
+        cnt=1;
     }
-    printf("=%d\n",d);
+    if(d!=0){
+        if(cnt==1 && d>0) printf("+");
+        printf("%d",d);
+    }
+    printf("=0\n");
 }
 
 void project(double *x,double *y,double *z,i32 a,i32 b,i32 c,i32 d){
