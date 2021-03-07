9655a0f54cc609ee9058baef0ea3f25e4a2616f7
aokblast <i19780219111@kimo.com>
2021-03-07 23:51:12 +0800

    (Code)Finish hw0103.c.

---
+++ hw02-01/hw0103.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "hw0103.h"
+
+
+void swap(char **p1, char **p2) {
+    char *tmp = *p1;
+    *p1 = *p2; 
+    *p2 = tmp;
+}
+
+
+int main(){
+    print_answers();
+    swap(&pStr01, &pStr02);
+    print_answers();
+}
