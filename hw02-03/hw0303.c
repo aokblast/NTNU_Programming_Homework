9690c69110a390a86bf8b789dc37d3be1508bacf
aokblast <i19780219111@kimo.com>
2021-04-17 21:26:24 +0800

    (Code)Adjust hw0303 header.

--- hw02-03/hw0303.c
+++ hw02-03/hw0303.c
@@ -1,29 +1,5 @@
 #include "all.h"
-
-typedef struct _sBmpHeader
-{
-    char		bm[2];
-    uint32_t	size;
-    uint32_t	reserve;
-    uint32_t	offset;
-    uint32_t	header_size;
-    uint32_t	width;
-    uint32_t	height;
-    uint16_t	planes;
-    uint16_t	bpp;
-    uint32_t	compression;
-    uint32_t	bitmap_size;
-    uint32_t	hres;
-    uint32_t	vres;
-    uint32_t	used;
-    uint32_t	important;
-}__attribute__ ((__packed__)) bmpHeader;
-
-typedef struct _pixel{
-    uint8_t b;
-    uint8_t g;
-    uint8_t r;
-}__attribute__ ((__packed__)) pixel;
+#include "bmp.h"
 
 int main(){
     char fileName[100];
@@ -44,16 +20,16 @@ int main(){
     fwrite(header, sizeof(bmpHeader), 1, writeFile);
     for(int i = 0; i < header->height; ++i) {
         int offset = (i) * sin(angle / 180);
-        pixel new_row[header->width], old_row[origWidth];
-        pixel white = {255, 255, 255};
-        fread(old_row, sizeof(pixel), origWidth, readFile);
+        pixel24 new_row[header->width], old_row[origWidth];
+        pixel24 white = {255, 255, 255};
+        fread(old_row, sizeof(pixel24), origWidth, readFile);
         for(int j = 0; j < header->width; ++j) {
             new_row[j] = white;
         }
         for(int j = 0; j < origWidth; ++j) {
             new_row[j + offset] = old_row[j];
         }
-        fwrite(new_row, sizeof(pixel), header->width, writeFile);
+        fwrite(new_row, sizeof(pixel24), header->width, writeFile);
     }
     fclose(readFile);
     fclose(writeFile);
