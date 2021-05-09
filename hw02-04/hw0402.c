4c00610ede49bac23c6176c754806092bc0991ef
aokblast <i19780219111@kimo.com>
2021-05-09 20:23:51 +0800

    (Code)Adjust level 1-3.

--- hw02-04/hw0402.c
+++ hw02-04/hw0402.c
@@ -1,16 +1,26 @@
 #include "all.h"
 
-void random_length_string(char *str, size_t length) {
-    for(int i = 0; i < length; ++i) {
+bool start_func = false;
+bool end_func  = false;
+bool is_end = false;
+
+char opers[] = "+!~*/&%<>=^|?:'";
+
+char *random_length_string(char *str, size_t length) {
+    int i = 0;
+    for(i = 0; i < length; ++i) {
         str[i] = rand() % 26 + 'a';
     }
+    str[i] = '\0';
+    return str;
 }
 
-int in_variables_index(char str[], char variable[][100]) {
+int in_variables_index(char str[], char variables[][100]) {
     for(int i = 0; i < 10000; ++i) {
-        if(strcmp(str, variable[i]) == 0) {
+        if(strcmp(str, variables[i]) == 0) {
+            
             return i;
-        }else if (variable[i][0] == '\0') {
+        }else if (variables[i][0] == '\0') {
             return -1;
         }
     }
@@ -21,101 +31,150 @@ bool is_function(char str[]) {
     return (strrchr(str, '(') != NULL && strrchr(str, ')') != NULL);
 }
 
+void get_line(char *str, fstream readFile) {
+    int index = 0;
+    
+    while(!feof(readFile)) {
+        int c = fgetc(readFile);
+        if(c == -1) {
+            str[index++] = '\0';
+            return;
+        }
+        if(c != ';' && c != '{' && c != '}') {
+            if(strchr(opers, c) != NULL) {
+                str[index++] = ' ';
+                str[index++] = c;
+                while(strchr(opers, (c = fgetc(readFile)) ) != NULL) {
+                    str[index++] = c;
+                }
+                str[index++] = ' ';
+            }else if(c == '(') {
+                while(str[--index] == ' ');
+                ++index;
+                str[index++] = '(';
+                str[index++] = ' ';
+                continue;
+            }else if (c == ')') {
+                str[index++] = ' ';
+            }else if( c == ',') {
+                str[index++] = ' ';
+                str[index++] = ',';
+                str[index++] = ' ';
+                continue;
+            }
+            if(c == '\n') c = ' ';
+            str[index++] = c;
+        }else {
+            if(c == ';') is_end = true;
+            else if(c == '{') start_func = true;
+            else if(c == '}') end_func = true;
+            str[index++] = '\0';
+            return;
+        }
+    }
+}
+
+void add_redundant(fstream writeFile) {
+    int times = rand() % 10;
+    while(times--) fwrite((rand() % 2 == 0) ? "\n" : " ", 1, 1, writeFile);
+}
+
+char *operator_obfs(char *str, int a) {
+     
+}
+
 
 void obfuscation(fstream readFile, fstream writeFile, int mode) {
     char variables[10000][100] = {0}, replacement[10000][100] = {0};
-    string word;
+    bool obfus_func = (mode >= 3 ? true : false);
+    bool obfus_variable = (mode >= 2 ? true : false);
+    bool obfus_redun = (mode >= 1 ? true : false);
+    bool obfus_oper = (mode >= 4 ? true : false);
+    string line;
     int top = 0;
     //skip pre-processor
     while(!feof(readFile)) {
         uint64_t lastPos = ftell(readFile);
-        fgets(word, 10005, readFile);
-        if(strncmp(word, "int", 3) == 0 || strncmp(word, "char", 4) == 0) {
+        fgets(line, 10005, readFile);
+        if(line[0] != '#' && line[0] != '\n') {
             fseek(readFile, lastPos, SEEK_SET);
             break;
         }else {
-            fwrite(word, 1, strlen(word), writeFile);
+            fwrite(line, 1, strlen(line), writeFile);
         }
     }
-
+    string word , tmp;
     //obfuscation
     while(!feof(readFile)) {
-        uint64_t lastPos = ftell(readFile);
-        fgets(word, 10005, readFile);
-        bool hasInter = false;
-        if(word[0] == '\n') {
-            continue;
-        }else {
-            fseek(readFile, lastPos, SEEK_SET);
-        }
-        fscanf(readFile, " %s", word);
-        if(word[strlen(word) - 1] == ';') {
-            word[strlen(word) - 1] = '\0';
-            hasInter = true;
-        }
-        int index = in_variables_index(word, variables);
-        char redun[1] = {((rand() % 2 ) == 0 ? ' ' : '\n')};
-        if(mode >= 1) {
-            fwrite(redun, 1, sizeof(char), writeFile);
-        }
-        if(strcmp(word, "char") == 0 || strcmp(word, "int") == 0) {
-            fwrite(word, 1, strlen(word), writeFile);
-            redun[0] = ((rand() % 2 ) == 0 ? ' ' : '\n');
-            hasInter = false;
-            if(mode >= 1) {
-                fwrite(redun, 1, sizeof(char), writeFile);
+        get_line(line, readFile);
+        bool isFunction = is_function(line);
+        if(line[0] == '\n') continue;
+        char *iter = strtok(line, " ");
+        if(iter == NULL) goto end;
+        if(strcmp(iter, "char") == 0 || strcmp(iter, "int") == 0) {
+            if(obfus_redun)add_redundant(writeFile);
+            fwrite(iter, 1, strlen(iter), writeFile);
+            iter = strtok(NULL, " ");
+            if(strcmp(iter, "main(") == 0) {
+                fwrite(" ", 1, 1, writeFile);
+                fwrite(iter, 1, strlen(iter), writeFile);
+                fwrite(")", 1, 1, writeFile);
+                goto end;
             }
-            fscanf(readFile, " %s", word);
-            if(mode >= 3) {
-
-                if(is_function(word)) {
-                    if(strncmp(word, "main", 4) == 0) {
-                        fwrite(word, 1, strlen(word), writeFile);
-                        continue;
+            if(obfus_redun)add_redundant(writeFile);
+            if((isFunction && obfus_func) || (!isFunction && obfus_variable)) {
+                strcpy(replacement[top], random_length_string(tmp, 16));
+                if(isFunction)strncpy(variables[top], iter, strchr(iter, '(') - iter);
+                else strcpy(variables[top], iter);
+                fwrite(replacement[top], 1, strlen(replacement[top]), writeFile);
+                if(isFunction) fwrite(strchr(iter, '('), 1, strlen(strchr(iter, '(')), writeFile);
+                while((iter = strtok(NULL, " ")) != NULL) {
+                    int index = in_variables_index(iter, variables);
+                    if(obfus_redun)add_redundant(writeFile);
+                    if(index != -1) {
+                        fwrite(replacement[index], 1, strlen(replacement[index]), writeFile);
+                    }else {
+                        fwrite(iter, 1, strlen(iter), writeFile);
                     }
-                    if(word[strlen(word) - 1] == ';') {
-                        word[strlen(word) - 1] = '\0';
-                        hasInter = true;
-                    }
-                    strcpy(variables[top], word);
-                    random_length_string(word, 16);
-                    strcpy(replacement[top], word);
-                    strcpy(&replacement[top][strlen(replacement[top])], strrchr(variables[top], '(' ));
-                    fwrite(replacement[top], 1, strlen(replacement[top]), writeFile);
-                    if(hasInter) {
-                        fwrite(";",1, 1, writeFile);
-                    }
-                    ++top;
                 }
-            } 
-            if(mode >= 2) {
-                if(word[strlen(word) - 1] == ';') {
-                    word[strlen(word) - 1] = '\0';
-                    hasInter = true;
+                 ++top;
+            }else {
+                if(obfus_redun)add_redundant(writeFile);
+                fwrite(iter, 1, strlen(iter), writeFile);
+                while((iter = strtok(NULL, " ")) != NULL) {
+                    if(obfus_redun)add_redundant(writeFile);
+                   fwrite(iter, 1, strlen(iter), writeFile); 
                 }
-                if(!is_function(word)) {
-                    strcpy(variables[top], word);
-                    random_length_string(word, 16);
-                    strcpy(replacement[top], word);
-                    fwrite(replacement[top], 1, strlen(replacement[top]), writeFile);
-                    if(hasInter) {
-                        fwrite(";",1, 1, writeFile);
-                    }
-                    ++top;
+            }
+           
+            
+        }else {
+            bool left = false;
+            do {
+                int index;
+                if(strchr(iter, '(') != NULL) {
+                    left = true;
+                    *(strchr(iter, '(')) = '\0';
+                }
+                
+                index = in_variables_index(iter, variables);
+                if(obfus_redun)add_redundant(writeFile);
+                if(index != -1) {
+                    fwrite(replacement[index], 1, strlen(replacement[index]), writeFile);
                 }else {
-                    fwrite(word, 1, strlen(word), writeFile);
+                    fwrite(iter, 1, strlen(iter), writeFile);
                 }
-            } else {
-                fwrite(word, 1, strlen(word), writeFile);
-            }
-        }else if(index != -1) {
-            fwrite(replacement[index], 1, strlen(replacement[index]), writeFile);
-            if(hasInter) fwrite(";",1, 1, writeFile);
-        }else {
-            fwrite(word, 1, strlen(word), writeFile);
-            if(hasInter) fwrite(";",1, 1, writeFile);
-        }
+                if(left) fwrite("(", 1, 1, writeFile), left = false;
+            }while((iter = strtok(NULL, " ")) != NULL);
+            
 
+        }
+        
+        if(obfus_redun)add_redundant(writeFile);
+        end:
+        if(is_end)fwrite(";", 1, 1, writeFile), is_end = false;
+        else if(start_func) fwrite("{", 1, 1, writeFile), start_func = false;
+        else if(end_func) fwrite("}", 1, 1, writeFile), end_func = false;
     }
 
     fseek(readFile, 0, SEEK_SET);
