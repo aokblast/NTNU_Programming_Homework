deb81f2aab6e4d87af126b93b3e8065c0be4b3be
aokblast <i19780219111@kimo.com>
2021-06-05 04:31:00 +0800

    (Code)Finish hw02-1 hw02-2.

--- NTU_DSA/Homework2/1.c
+++ NTU_DSA/Homework2/1.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+int min[1000005] = {0};
+int max[1000005] = {0};
+
 int main() {
-    
+    int totalCase = 0, num = 0, left = 0, right = 0, ans = 0;
+    scanf(" %d", &totalCase);
+    memset(max, 127, 4 * 1000005);
+    for(int i = 1; i <= totalCase; ++i) {
+        scanf(" %d %d %d", &num, &left, &right);
+        min[(right == -1 ? 0 : right)] = (min[i] < num ? num : min[i]);
+        max[(right == -1 ? 0 : right)] = max[i];
+        max[(left == -1 ? 0 : left)] = (max[i] > num ? num : max[i]);
+        min[(left == -1 ? 0 : left)] = min[i];
+        if( min[i] <= num && num <= max[i]) ++ans;
+    }
+    printf("%d\n", ans);
 }
+
