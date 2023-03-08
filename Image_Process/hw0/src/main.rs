0836d2a1e0b76758efc83217ea812bfd401068d6
aokblast <i19780219111@kimo.com>
2023-03-09 01:51:28 +0800

    feat: output with file

--- Image_Process/hw0/src/main.rs
+++ Image_Process/hw0/src/main.rs
@@ -1,25 +1,31 @@
-use std::ffi::c_void;
-use opencv::core::{CV_8U, hconcat, Mat, Mat_AUTO_STEP, MatTraitConst, Vec3b};
+use opencv::prelude::*;
+use opencv::core::{CV_8UC3, hconcat2, Mat, Scalar, Vec3b, Vector};
 use opencv::highgui::{destroy_all_windows, imshow, wait_key};
-use opencv::imgcodecs::{imread, IMREAD_COLOR};
+use opencv::imgcodecs::{imread, IMREAD_COLOR, imwrite};
 
 fn main() {
-    let img = imread("image.png", IMREAD_COLOR).unwrap();
-    imshow("Image", &img).unwrap();
+    let img = imread("ccw.jpg", IMREAD_COLOR).unwrap();
 
-    let mut gray_scale = vec![0; (img.rows() * img.cols()) as usize];
+    let mut gray_scale_img = Mat::new_size_with_default(
+        img.size().unwrap(), CV_8UC3, Scalar::all(0 as f64)).unwrap();
 
     for r in 0..img.rows() {
         for c in 0..img.cols() {
             let pixel= img.at_2d::<Vec3b>(r, c).unwrap();
-            gray_scale[(r * img.cols() + c) as usize] = ((pixel[0] as i32 + pixel[1] as i32 + pixel[2] as i32 ) / 3) as u8;
+            let cur = ((pixel[0] as i32 + pixel[1] as i32 + pixel[2] as i32 ) / 3) as u8;
+            let cur_pixel = gray_scale_img.at_2d_mut::<Vec3b>(r, c).unwrap();
+            cur_pixel[0] = cur;
+            cur_pixel[1] = cur;
+            cur_pixel[2] = cur;
         }
     }
 
-    unsafe {
-        let gray_scale_img = Mat::new_rows_cols_with_data(img.rows(), img.cols(), CV_8U, gray_scale.as_mut_ptr() as *mut c_void, Mat_AUTO_STEP).unwrap();
-        imshow("Gray_Scale", &gray_scale_img).unwrap();
-    }
+    let mut res = Mat::new_rows_cols_with_default(img.rows(), img.cols() * 2, CV_8UC3, Scalar::all(0 as f64)).unwrap();
+
+    hconcat2(&img, &gray_scale_img, &mut res).unwrap();
+
+    imshow("Image", &res).unwrap();
+    imwrite("res.jpg", &res, &Vector::<i32>::new()).unwrap();
 
 
 
