d6e72dc6073771a68cda8b9701e94993dc1f222f
aokblast <i19780219111@kimo.com>
2023-03-23 15:04:35 +0800

    feat: add Real Time System HW1 plotter support for multiple files

--- Real_Time_System/HW1/plotter/src/main.rs
+++ Real_Time_System/HW1/plotter/src/main.rs
@@ -1,9 +1,12 @@
 use ndhistogram::{ndhistogram, axis::Uniform, Histogram};
 use plotters::prelude::*;
 use std::{env, io::{BufReader, BufRead}, fs::File};
+use std::cmp::{max, min};
 
 const BINS: usize = 128;
 const GIGA: f64 = 1e6;
+
+const COLORS: [RGBColor; 7] = [BLUE, RED, MAGENTA, CYAN, GREEN, BLACK, YELLOW];
 
 fn to_cdf(data: &Vec<u64>) -> Vec<(f64, f64)> {
     let mut hist = ndhistogram!(Uniform::new(BINS, *data.iter().min().unwrap() as f64 , (*data.iter().max().unwrap() + 1) as f64));
@@ -45,23 +48,29 @@ fn to_cdf(data: &Vec<u64>) -> Vec<(f64, f64)> {
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
-    let input_file = &args[1];
-    let output_file = &args[2];
+    let output_file = &args[1];
+    let input_files = &args[2..min(args.len(), COLORS.len())];
 
-    // Collecting Data
-    let file_reader = BufReader::new(File::open(input_file).unwrap());
+    let mut data = vec![];
+    let mut boundary = (u64::MAX, u64::MIN);
 
-    let mut data: Vec<u64> = vec![];
+    for file in input_files {
+        // Collecting Data
+        let mut d: Vec<u64> = vec![];
+        let file_reader = BufReader::new(File::open(file).unwrap());
 
-    for line in file_reader.lines() {
-        if let Ok(line) = line {
-            if let Ok(num) = line.parse() {
-                data.push(num);
+        for line in file_reader.lines() {
+            if let Ok(line) = line {
+                if let Ok(num) = line.parse() {
+                    d.push(num);
+                    boundary.0 = min(boundary.0, num);
+                    boundary.1 = max(boundary.1, num);
+                }
             }
         }
-    }
 
-    let cdf = to_cdf(&data);
+        data.push(d);
+    }
 
     // Draw Graph
     let root = BitMapBackend::new(output_file, (1024, 768)).into_drawing_area();
@@ -72,23 +81,31 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .set_label_area_size(LabelAreaPosition::Left, 60)
         .set_label_area_size(LabelAreaPosition::Bottom, 60)
         .caption("P1_Result", ("sans-serif", 40))
-        .build_cartesian_2d((*data.iter().min().unwrap() as f64 / GIGA)..(*data.iter().max().unwrap() as f64 / GIGA), 0.0..1.0)?;
+        .build_cartesian_2d((boundary.0 as f64 / GIGA)..((boundary.1 + 1) as f64 / GIGA), 0.0..1.0)?;
 
     chart.configure_mesh()
         .disable_x_mesh()
         .disable_y_mesh()
         .x_desc("Response Time (ms)")
-        .y_desc("Cumulative Possibility")
+        .y_desc("Cumulative Probability")
         .axis_desc_style(("sans-serif", 25))
         .draw()?;
 
-    chart.draw_series(
-        AreaSeries::new(
-            cdf.iter().map(|item| { (item.0, item.1) }),
-            0.0,
-            &RED.mix(0.2),
-        ).border_style(&RED),
-    )?;
+    let mut idx = 0;
+
+    for d in data {
+        let cdf = to_cdf(&d);
+        let cur = idx;
+        chart.draw_series(
+            LineSeries::new(
+                cdf.iter().map(|item| { (item.0, item.1) }),
+                &COLORS[idx],
+            ))?
+            .label(format!("P{}", idx + 1)).legend(move |(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &COLORS[cur]));
+        idx += 1;
+    }
+
+    chart.configure_series_labels().border_style(&BLACK).draw()?;
 
     root.present().unwrap();
     println!("Result has been saved to {}", output_file);
